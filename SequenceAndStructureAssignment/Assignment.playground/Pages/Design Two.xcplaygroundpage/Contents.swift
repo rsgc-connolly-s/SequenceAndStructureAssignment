5725c368e2ce03b2c74fe1c4a5103051e101e8dc
Sean <student@macbook-pro-405-connolly.local>
2017-03-03 16:54:54 -0500

    Made it more complicated

--- SequenceAndStructureAssignment/Assignment.playground/Pages/Design Two.xcplaygroundpage/Contents.swift
+++ SequenceAndStructureAssignment/Assignment.playground/Pages/Design Two.xcplaygroundpage/Contents.swift
@@ -50,14 +50,18 @@ for x in stride(from: 20, through: 500, by: 50){
     for y in stride(from:20, through: 475, by: 50){
         let number = random(from: 0, toButNotIncluding: 2)
         if number == 0{
-            canvas.drawShapesWithFill = false
-            canvas.drawShapesWithBorders = true
-            canvas.borderColor = Color(hue: MC, saturation: 100, brightness: 100, alpha: 100)
-            canvas.defaultBorderWidth = 1
-            canvas.drawRectangle(bottomLeftX: x, bottomLeftY: y, width: 10, height: 10)
-            canvas.drawRectangle(bottomLeftX: x-5, bottomLeftY: y-5, width: 20, height: 20)
+            for s in stride(from: 20, through: 10, by: -5){
+                let start = random(from: -2, toButNotIncluding: 3)
+                canvas.drawShapesWithFill = false
+                canvas.drawShapesWithBorders = true
+                canvas.borderColor = Color(hue: MC, saturation: 100, brightness: 100, alpha: 100)
+                canvas.defaultBorderWidth = 1
+                canvas.drawRectangle(bottomLeftX: x-start, bottomLeftY: y-start, width: s, height: s)
+                
+                }
+            
         }else{
-            for s in stride(from: 5, through: 30, by: 5){
+            for s in stride(from: 5, through: 25, by: 5){
                 let center = random(from: 0, toButNotIncluding: 3)
                 canvas.drawShapesWithFill = false
                 canvas.drawShapesWithBorders = true
@@ -86,33 +90,28 @@ for x in stride(from: 100, through:300, by: 100){
 }
 
 //Draw Inner Circle Pattern
-canvas.borderColor = Color(hue: SC, saturation: 100, brightness: 100, alpha: 100)
-canvas.defaultBorderWidth = 2
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 90, height: 90)
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 45, height: 45)
 
-canvas.drawEllipse(centreX: 250, centreY: 150, width: 90, height: 90)
-canvas.drawEllipse(centreX: 250, centreY: 150, width: 45, height: 45)
-
-canvas.drawEllipse(centreX: 350, centreY: 250, width: 90, height: 90)
-canvas.drawEllipse(centreX: 350, centreY: 250, width: 45, height: 45)
-
-canvas.drawEllipse(centreX: 250, centreY: 350, width: 90, height: 90)
-canvas.drawEllipse(centreX: 250, centreY: 350, width: 45, height: 45)
-
+for x in stride(from: 90, through: 10, by: -5){
+    canvas.borderColor = Color(hue: SC, saturation: 100, brightness: 100, alpha: 100)
+    canvas.defaultBorderWidth = 2
+    canvas.drawEllipse(centreX: 150, centreY: 250, width: x, height: x)
+    canvas.drawEllipse(centreX: 250, centreY: 150, width: x, height: x)
+    canvas.drawEllipse(centreX: 350, centreY: 250, width: x, height: x)
+    canvas.drawEllipse(centreX: 250, centreY: 350, width: x, height: x)
+}
 
 //Draw inner Square Pattern
-canvas.borderColor = Color(hue: MC, saturation: 100, brightness: 100, alpha: 100)
-canvas.defaultBorderWidth = 25
-canvas.drawRectangle(bottomLeftX: 123, bottomLeftY: 123, width: 55, height: 55)
 
-canvas.drawRectangle(bottomLeftX: 323, bottomLeftY: 323, width: 55, height: 55)
+for x in stride(from: 55, through: 5, by: -5){
+    canvas.borderColor = Color(hue: MC, saturation: 100, brightness: 100, alpha: 100)
+    canvas.defaultBorderWidth = 25
+    canvas.drawRectangle(bottomLeftX: 123, bottomLeftY: 123, width: x, height: x)
+    canvas.drawRectangle(bottomLeftX: 323, bottomLeftY: 323, width: x, height: x)
+    canvas.drawRectangle(bottomLeftX: 123, bottomLeftY: 323, width: x, height: x)
+    canvas.drawRectangle(bottomLeftX: 323, bottomLeftY: 123, width: x, height: x)
+    canvas.drawRectangle(bottomLeftX: 223, bottomLeftY: 223, width: x, height: x)
+    }
 
-canvas.drawRectangle(bottomLeftX: 123, bottomLeftY: 323, width: 55, height: 55)
-
-canvas.drawRectangle(bottomLeftX: 323, bottomLeftY: 123, width: 55, height: 55)
-
-canvas.drawRectangle(bottomLeftX: 223, bottomLeftY: 223, width: 55, height: 55)
 
 //Copy to Clipboard
 canvas.copyToClipboard()
